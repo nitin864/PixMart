707dc159f540ac068061d21cf1c13aa17124a23d
nitin864 <rajnitin793@gmail.com>
2026-04-04 11:09:22 +0530

    user data stored to cookies in browser

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
   
 import "./globals.css";
+import Provider from "@/Provider";
 
  
 
@@ -19,7 +20,9 @@ export default function RootLayout({
       <body
           
       >
+        <Provider>
         {children}
+        </Provider>
       </body>
     </html>
   );
